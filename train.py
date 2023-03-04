6144d6760f31a320c0aa3e98f7788611dbb6e313
jh868 <2weeks9947@naver.com>
2023-03-04 10:14:22 +0900

    fix

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ from torch.utils.data.dataloader import DataLoader
 import cv2
 
 from model import MobileUNet
+import matplotlib.pyplot as plt
 
 
 class Human(Dataset):
@@ -36,13 +37,16 @@ class Human(Dataset):
     def preprocessing_mask(self, mask):
         # mask = mask.resize(self.input_size)
         # mask = np.resize(mask, self.input_size)
-        mask = self.transform_mask(image=mask)['image']
-        mask = mask.astype(np.float32)
 
         mask[mask < 255] = 0
         mask[mask == 255.0] = 1
+        mask = mask.astype(np.float32)
+        mask = self.transform(image=mask)['image']
 
-        mask = torch.tensor(mask)
+        # mask[mask < 255] = 0
+        # mask[mask == 255.0] = 1
+
+        # mask = torch.tensor(mask)
         mask = mask.squeeze()
         return mask
 
@@ -50,6 +54,8 @@ class Human(Dataset):
         X_train = cv2.imread(self.X_train[i])
         X_train = X_train.astype(np.float32)
         X_train = self.transform(image=X_train)['image']
+        # X_train = torch.tensor(X_train)
+
         Y_train = cv2.imread(self.Y_train[i], cv2.IMREAD_GRAYSCALE)
         Y_train = self.preprocessing_mask(Y_train)
 
@@ -60,25 +66,17 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 transform = A.Compose([
     A.Resize(width=256, height=256),
-    A.HorizontalFlip(),
-    A.RandomShadow(),
-    A.RandomBrightnessContrast(),
-    A.RandomCrop(192, 192),
+    A.HorizontalFlip(p=0.5),
+    A.Rotate(limit=30, p=0.3),
     ToTensorV2()
-])
-transform_mask = A.Compose([
-    A.Resize(width=256, height=256),
-    A.HorizontalFlip(),
-    A.RandomCrop(192, 192),
 ])
 
 train_set = Human(path_to_img='D:seg/image/',
                   path_to_anno='D:seg/mask/',
                   transfrom=transform,
-                  transform_mask=transform_mask
                   )
 
-train_loader = DataLoader(train_set, batch_size=120, shuffle=True)
+train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
 
 model = MobileUNet().to(device)
 
@@ -107,85 +105,16 @@ for epoch in range(200):
 
         iterator.set_description(f'epoch: {epoch + 1} loss: {loss.item()}')
 
-# save checkpoint
-    if (epoch+1) % 5 == 0:
+    # save checkpoint
+    if (epoch + 1) % 5 == 0:
         torch.save({
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optim.state_dict(),
             'epoch': epoch
-        }, f'Portrait_seg_{epoch+1}.pth')
+        }, f'Portrait_seg_{epoch + 1}.pth')
 
 torch.save({
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optim.state_dict(),
-            'epoch': 200
-        }, 'Portrait_segmentation.pth')
-
-model.load_state_dict(torch.load('Human_seg_full_50.pth', map_location='cpu'))
-
-img_path = './seg/image/1803151818-00000003.jpg'
-
-img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-
-img_copy = cv2.resize(img, (256, 256))
-
-img_copy2 = np.transpose(img_copy, (2, 0, 1))
-img_copy2 = torch.unsqueeze(torch.tensor(img_copy2), 0)
-
-pred = model(img_copy2.float())
-pred = pred.detach().numpy()
-pred = pred.astype(np.uint8)
-print("pred = ", pred.shape)
-
-_, mask = cv2.threshold(pred, 200, 255, cv2.THRESH_BINARY)
-mask = cv2.bitwise_not(mask)  # 반전
-
-# cv2로 투명배경 멕이기 # 근데 생각해보니까 투명 배경할 이유가없음
-# mask = cv2.merge((mask, mask, mask))
-# result = cv2.addWeighted(img_copy, 1, mask, 1, 0)
-
-b, g, r = cv2.split(img_copy)
-result = cv2.merge([b, g, r, pred], 4)
-print("result= ", result.shape)
-
-cv2.imwrite('result.png', result)
-cv2.imshow(' ', result)
-cv2.waitKey(0)
-
-# 배경관련
-fg_h, fg_w, _ = img.shape
-
-background = cv2.imread('background.jpg')
-
-bg_h, bg_w, _ = background.shape
-
-# fit to fg width
-background = cv2.resize(background, dsize=(fg_w, int(fg_w * bg_h / bg_w)))
-
-bg_h, bg_w, _ = background.shape
-
-margin = (bg_h - fg_h) // 2
-
-if margin > 0:
-    background = background[margin:-margin, :, :]
-else:
-    background = cv2.copyMakeBorder(background, top=abs(margin), bottom=abs(margin), left=0, right=0,
-                                    borderType=cv2.BORDER_REPLICATE)
-
-# final resize
-background = cv2.resize(background, dsize=(fg_w, fg_h))
-
-cv2.imshow("", background)
-cv2.waitKey(0)
-
-# 넣기
-mask = cv2.resize(mask, (fg_w, fg_h))
-
-print(background.shape)
-print(img.shape)
-print(mask.shape)
-
-cv2.copyTo(img, mask, background)
-
-cv2.imshow("", background)
-cv2.waitKey(0)
+    'model_state_dict': model.state_dict(),
+    'optimizer_state_dict': optim.state_dict(),
+    'epoch': 200
+}, 'Portrait_segmentation.pth')
